47995cf5a6adac55ee5c7d341f19aa1270305789
Shristi Pokhrel <shristipok2000@gmail.com>
2023-04-10 14:07:23 +0545

    just basic

---
+++ basic/src/UseState.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+const UseState = () => {
+    const [count,setCount]=useState(0);
+    function handleChange(){
+        setCount(count+1)
+    }
+    function decChange(){
+        if(count===0){
+            setCount(0)
+        }
+        else{
+            setCount(count-1)
+        }
+    }
+  return (
+    <>
+     <div className='count'>
+        <button onClick={decChange} >-</button>
+        <p>Count:{count}</p>
+        {/* <button onClick={(e)=>setCount(count+1)}>+</button> */}
+         <button onClick={handleChange}>+</button>
+     </div>
+    </>
+  )
+}
+
+export default UseState
+
+
